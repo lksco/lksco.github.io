5474c5a356b22fe17ab34f7ac1966ffa9e869d18
monkeysHK <monkeyshk-main@protonmail.com>
2022-07-26 00:06:50 +0800

    translations

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,18 @@
 /* https://stackoverflow.com/questions/19912375/javascript-typing-effect */
 function type(i, t, oe, text) {
     var input = text;
-    document.getElementById(oe).innerHTML += input.charAt(i);
-    setTimeout(function(){
+    document.querySelector(oe).innerHTML += input.charAt(i);
+    setTimeout(function () {
         if (i < input.length - 1)
-            type(i+1, t, oe, text);
-    }, t);
+            type(i + 1, t, oe, text);
+    }, t / input.length);
 }
 
 setTimeout(function () {
-    type(0, 100, "typeout-1", "Documentation for LKSCO users");
-    type(0, 100, "typeout-2", "Documentation for other users and developers");
+    type(0, 3000, "#typeout-1 .lang1", "Documentation for LKSCO users");
+    type(0, 3000, "#typeout-2 .lang1", "Documentation for other users and developers");
+    setTimeout(function () {
+        type(0, 2000, "#typeout-1 .lang2", "為本校中樂團而設之說明");
+        type(0, 2000, "#typeout-2 .lang2", "為其他用家或開發者而設之說明");
+    }, 3500);
 }, 1000);
